a7d7869c07b2c4ed43d934c058f5d4dd71599392
MuhammadShabrani <mohammad.a.shabrani@gmail.com>
2023-07-11 23:12:48 +0300

    an-update-for-the-code

---
+++ main.js
@@ -0,0 +1,32 @@
+const divs = document.querySelectorAll('.tasks');
+const labels = document.querySelectorAll('.label');
+const icons = document.querySelectorAll('.icon');
+
+for (let i = 0; i < labels.length; i++) {
+  labels[i].addEventListener('click', function() {
+    divs[i].classList.toggle('active');
+
+    if (divs[i].classList.contains('active')) 
+      icons[i].textContent = '▲';
+     else 
+      icons[i].textContent = '▼';
+  });
+}
+
+
+
+let a = function (){
+  console.log('diid,')
+}
+
+a()
+
+
+
+let person = {
+  name:'name',
+  func: function (){
+    console.log(this.name)
+  }
+
+}
